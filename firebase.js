9a2526672ac2f1dd1f4e264c63a7911adb1098b6
jaataj293-commits <jaataj293@gmail.com>
2026-07-20 21:24:29 +0530

    Update Firebase configuration with new credentials

--- firebase.js
+++ firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB_JZcLlhZdD3J5S98vKHmQjWG_g1jUQHI",
+  authDomain: "fitarc-3d80b.firebaseapp.com",
+  projectId: "fitarc-3d80b",
+  storageBucket: "fitarc-3d80b.firebasestorage.app",
+  messagingSenderId: "689550926779",
+  appId: "1:689550926779:web:6dd4c005a5e2c235fa1a9e",
+  measurementId: "G-4ST22D9802"
 };
 
 const app = initializeApp(firebaseConfig);
