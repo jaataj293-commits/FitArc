0e480948fc654580c2072ea975895e120f2f5a30
jaataj293-commits <jaataj293@gmail.com>
2026-07-22 15:35:48 +0530

    Update Firebase imports to use CDN links

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB_JZcLlhZdD3J5S98vKHmQjWG_g1jUQHI",
